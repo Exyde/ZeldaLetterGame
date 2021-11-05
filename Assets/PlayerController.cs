5af792486668de16b6f1f122cf3d67e61818c26b
Exyde <54008677+Exyde@users.noreply.github.com>
2021-11-05 22:06:17 +0100

    Controller Fix

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,34 +6,36 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject _LetterPrefab;
     public GameObject _cursor;
-    
-
     public Transform[] _anchors;
 
     public Vector2 XBound, YBound;
     float cursorStep = 2.5f;
 
-    void Start()
-    {
-        _cursor.transform.position = _anchors[0].position;
+    void Start(){
+        _cursor.transform.localPosition = _anchors[0].position;
     }
-    void Update()
-    {
+    void Update(){
         HandleCursor();
     }
 
     private void HandleCursor(){
-        if (Input.GetKeyDown(KeyCode.Q) && _cursor.transform.position.x > XBound.x ){
-            _cursor.transform.position += new Vector3 ( -cursorStep, 0, 0);
+        if (Input.GetKeyDown(KeyCode.Q) && _cursor.transform.localPosition.x > XBound.x ){
+            _cursor.transform.localPosition += new Vector3 ( -cursorStep, 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.D) && _cursor.transform.position.x < XBound.y ){
-            _cursor.transform.position += new Vector3 ( cursorStep, 0, 0);
+        if (Input.GetKeyDown(KeyCode.D) && _cursor.transform.localPosition.x < XBound.y ){
+            _cursor.transform.localPosition += new Vector3 ( cursorStep, 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.Z) && _cursor.transform.position.y < YBound.x ){
-            _cursor.transform.position += new Vector3 ( 0, cursorStep, 0);
+        if (Input.GetKeyDown(KeyCode.S) && _cursor.transform.localPosition.y > YBound.x ){
+            print(_cursor.transform.localPosition);
+            _cursor.transform.localPosition += new Vector3 (0, -cursorStep,  0);
+            print(_cursor.transform.localPosition);
+
         }
-        if (Input.GetKeyDown(KeyCode.S) && _cursor.transform.position.y > YBound.y ){
-            _cursor.transform.position += new Vector3 (0, -cursorStep,  0);
+        if (Input.GetKeyDown(KeyCode.Z) && _cursor.transform.localPosition.y < YBound.y ){
+            print(_cursor.transform.localPosition);
+            _cursor.transform.localPosition += new Vector3 ( 0, cursorStep, 0);
+            print(_cursor.transform.localPosition);
+
         }
     }
     
