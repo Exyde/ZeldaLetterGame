689ef6a495817c0644a527af8df0500108bab455
Exyde <54008677+Exyde@users.noreply.github.com>
2021-11-05 18:44:11 +0100

    Work

---
+++ Assets/Letter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Letter : MonoBehaviour
+{
+    public enum LetterType{ Ring, Flare, Magic, Bubbles, Spark, Flower}
+    public Material[] _materials;
+    public LetterType _letterType;
+    public GameObject FaceA;
+    public GameObject FaceB;
+    Rigidbody rb;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        SetRandomType();
+        SetMaterialBasedOnType();
+    }
+
+    void SetRandomType (){
+        _letterType = (LetterType)(Random.Range(0, 5));
+    }
+
+    void SetMaterialBasedOnType(){
+       FaceA.GetComponent<MeshRenderer>().material = _materials[(int)_letterType];
+       FaceB.GetComponent<MeshRenderer>().material = _materials[(int)_letterType];
+    }
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject _LetterPrefab;
+
+    public Transform[] _anchors;
+    
+}
